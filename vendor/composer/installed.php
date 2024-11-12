9fb51870234fe51f0c66b35dee3ca10a1202bf67
Панков Максим <maksim.pankov@dom.ru>
2024-11-12 16:06:01 +0300

    Add the index controller

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '269e985fa681e2ae6d942353ce56e98f94d84320',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '269e985fa681e2ae6d942353ce56e98f94d84320',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
